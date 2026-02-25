10e98b3914b4cf9901ce6afb9772c397555219e4
15538120205 <liu241023@qq.com>
2026-02-25 22:53:36 +0800

    订单支付业务代码开发

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -4,6 +4,8 @@ import com.sky.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.HashMap;
+
 @Mapper
 public interface UserMapper {
     /**
@@ -18,4 +20,21 @@ public interface UserMapper {
      * @param user
      */
     void insert(User user);
+
+
+
+    /**
+     * 根据id查询用户信息
+     *
+     * @param userId
+     * @return
+     */
+    User getById(Long userId);
+
+    /**
+     * 根据动态条件获取用户数
+     *
+     * @param map
+     */
+    Integer getUser(HashMap<String, Object> map);
 }
